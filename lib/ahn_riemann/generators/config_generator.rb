6268cc30d3b92f54f389375f1bec95d5ba648f8d
Fernando Alonso <krakatoa1987@gmail.com>
2013-04-16 09:58:10 -0300

    Improved config generator asking for stages and config

--- lib/ahn_riemann/generators/config_generator.rb
+++ lib/ahn_riemann/generators/config_generator.rb
@@ -1,13 +1,28 @@
 module AhnRiemann
   class ConfigGenerator < Adhearsion::Generators::Generator
-    argument :host, :type => :string, :default => "localhost"
-    argument :port, :type => :numeric, :default => 5555
+    argument :environments, :type => :array, :default => ["development"]
+
+    def self.source_root
+      File.join(File.dirname(File.expand_path(__FILE__)), "config/templates")
+    end
+
+    def load_config
+      @config = {}
+      @environments.each do |env|
+        @config[env] = {}
+        @config[env]["host"] = ask("Riemann server host (#{env})? [defaults to localhost]")
+        @config[env]["host"] = "localhost" if @config[env]["host"].empty?
+
+        @config[env]["port"] = ask("Riemann server port (#{env})? [defaults to 5555]")
+        @config[env]["port"] = "5555" if @config[env]["port"].empty?
+      end
+    end
 
     def gen_config
       self.destination_root = Adhearsion.config.platform.root
-      AhnRiemann::ConfigGenerator.source_root File.join(File.dirname(File.expand_path(__FILE__)), "config/templates")
       
       template "config/riemann.yml"
     end
+
   end
 end
